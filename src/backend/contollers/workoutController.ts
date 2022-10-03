5c0a1798675f5f37837e25052f528056dcfa56a6
David Bouckaert <>
2022-10-03 22:22:46 +0200

    finished post controller

--- src/backend/contollers/workoutController.ts
+++ src/backend/contollers/workoutController.ts
@@ -1,20 +1,42 @@
-//import Workout from 'the model'
-//import {workoutModel} from 'interfaces'
-import {Request, Response} from 'express'
+import WorkoutModel from '../interfaces/workoutModel.interface'
+import { Request, Response } from 'express'
+import Workout from '../models/workout.model'
+import { Document } from 'mongoose'
 
-const workout_index = (req:Request, res:Response)=>{
+const workout_index = (req: Request, res: Response) => {
     res.json({ message: 'GET all workouts' })
 }
-const workout_post = (req:Request, res:Response)=>{
-    res.json({ message: 'CREATE a workout' })
+const workout_post = async (req: Request, res: Response) => {
+    const { title, reps, load }: WorkoutModel = req.body
+    try {
+        const workout = await Workout.create({
+            title,
+            load,
+            reps
+        })
+        res.status(201).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error})
+        console.log(error)
+    }
+    /* const workout: WorkoutModel = new Workout(
+        req.body
+    )
+    workout.save()
+        .then((result) => {
+            console.log('New workout added to the database')
+        })
+        .catch((err) => {
+            console.log(err)
+        }) */
 }
-const workout_id_get = (req:Request, res:Response)=>{
+const workout_id_get = (req: Request, res: Response) => {
     res.json({ message: 'GET a single workout' })
 }
-const workout_id_delete = (req:Request, res:Response)=>{
+const workout_id_delete = (req: Request, res: Response) => {
     res.json({ message: 'DELETE a workout' })
 }
-const workout_id_patch = (req:Request, res:Response)=>{
+const workout_id_patch = (req: Request, res: Response) => {
     res.json({ message: 'PARTIAL UPDATE a workout' })
 }
 
