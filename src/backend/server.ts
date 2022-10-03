56d4354a7510835f8de4a84c2aca6943ad3f4adb
David Bouckaert <>
2022-10-03 22:22:27 +0200

    fixed bug: app.use(json) was below the routes

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -4,7 +4,9 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import fs from 'fs'
 import workoutRouter from './routes/workoutRoutes'
+import mongoose, { ConnectOptions } from 'mongoose'
 require('dotenv').config()
+const MONGO_URI: string = process.env.WORKOUT_MONGO_ATLAS_CONNECTION_STRING
 
 // create app
 const app: Application = express()
@@ -20,13 +22,20 @@ app.use(morgan('combined', {
     stream: fs.createWriteStream('./access.log', { flags: 'a' })
 }));
 
-// import routes
-app.use('/api/workouts', workoutRouter)
+// connect to db
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions).then(() => {
+    console.log('Connected to MongoDB!')
+    // listen on port 3000
+    app.listen(process.env.PORT, () => {
+        console.log(`listening on port ${process.env.PORT}`)
+    })
+}).catch((err:any) => { console.log(err) })
+
+// this takes all the URL encoded data and passes that into an object (body) that you can use on the request object
+app.use(express.urlencoded({extended:true}))
 
 // access JSON on the response (res).body property
 app.use(express.json())
 
-// listen on port 3000
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+// import routes
+app.use('/api/workouts', workoutRouter)
