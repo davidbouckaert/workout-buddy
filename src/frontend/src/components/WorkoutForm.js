21a543379a34d720b5ebab317224f6afe1f48916
David Bouckaert <>
2022-10-06 21:02:38 +0200

    using context providers and hooks: make the home page components responsive on change

--- src/frontend/src/components/WorkoutForm.js
+++ src/frontend/src/components/WorkoutForm.js
@@ -1,60 +1,63 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 const WorkoutForm = () => {
-    const [title, setTitle] = useState('')
-    const [load, setLoad] = useState('')
-    const [reps, setReps] = useState('')
-    const [error, setError] = useState(null)
+  const { dispatch } = useWorkoutsContext();
+  const [title, setTitle] = useState('');
+  const [load, setLoad] = useState('');
+  const [reps, setReps] = useState('');
+  const [error, setError] = useState(null);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const workout = { title, load, reps }
-        const response = await fetch('http://localhost:4000/api/workouts', {
-            method: 'POST',
-            body: JSON.stringify(workout),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const workout = { title, load, reps };
+    const response = await fetch('http://localhost:4000/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-        const json = await response.json()
-        if (!response.ok) {
-            setError(json.error)
-        }
-        if (response.ok) {
-            setTitle('')
-            setLoad('')
-            setReps('')
-            setError(null)
-            console.log('new workout added', json)
-        }
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
     }
+    if (response.ok) {
+      setTitle('');
+      setLoad('');
+      setReps('');
+      setError(null);
+      console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
+    }
+  };
 
-    return (
-        <form className='create' onSubmit={handleSubmit}>
-            <h3>Add a new workout</h3>
-            <label>Excersize title:</label>
-            <input
-                type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-            />
-            <label>Reps:</label>
-            <input
-                type="number"
-                onChange={(e) => setReps(e.target.value)}
-                value={reps}
-            />
-            <label>Load (kg):</label>
-            <input
-                type="number"
-                onChange={(e) => setLoad(e.target.value)}
-                value={load}
-            />
-            <button>Add Workout</button>
-            {error && <div className='error'>{error}</div>}
-        </form>
-    )
-}
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a new workout</h3>
+      <label>Excersize title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label>Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <label>Load (kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
 
-export default WorkoutForm
+export default WorkoutForm;
--- src/frontend/src/pages/home.js
+++ src/frontend/src/pages/home.js
@@ -1,38 +1,39 @@
-import {useEffect, useState} from 'react'
+import { useEffect } from 'react';
 
 // components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext();
 
-    const [workouts, setWorkouts] = useState(null)
+  // fetch the workouts only once by feeding it an empty array
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:4000/api/workouts');
+      const json = await response.json();
 
-    // fetch the workouts only once by feeding it an empty array
-    useEffect(()=>{
-        const fetchWorkouts = async () => {
-            const response = await fetch('http://localhost:4000/api/workouts')
-            const json = await response.json()
+      if (response.ok) {
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
+      }
+    };
 
-            if(response.ok){
-                setWorkouts(json) // array of workouts
-            }
-        }
+    fetchWorkouts();
+  }, [dispatch]);
 
-        fetchWorkouts()
-    },[])
+  /* only if workouts has a value, start mapping trough them. */
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
 
-    /* only if workouts has a value, start mapping trough them. */
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout)=>(
-                    <WorkoutDetails key={workout._id} workout={workout} />
-                ))}
-            </div>
-                <WorkoutForm/>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
